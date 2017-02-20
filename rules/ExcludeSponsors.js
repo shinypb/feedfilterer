5b339acd0e3232a4adfb0b159d5d082f81be9451
Mark Christian <m@rkchristian.ca>
2017-02-19 17:25:08 -0800

    Remove rules

--- rules/ExcludeSponsors.js
+++
@@ -1,6 +0,0 @@
-module.exports = function ExcludeSponsors(item) {
-    if (item.title.indexOf('RSS Sponsor:') === 0) return true;
-    if (item.title.indexOf('(Sponsor)') >= 0) return true;
-    if (item.title.indexOf('Sponsor – ') === 0) return true;
-    return false;
-}

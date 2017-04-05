9e612b6cd61025746f590a174e66e2a42b1078e0
Mark Christian <m@rkchristian.ca>
2017-04-04 21:00:01 -0300

    Gracefully handle items with empty fields

--- FeedItem.js
+++ FeedItem.js
@@ -1,8 +1,10 @@
 module.exports = class FeedItem {
     constructor(itemId, title, content, itemUrl) {
+        if (!itemId) throw new Error('Item must have an ID');
+
         this.itemId = itemId;
-        this.title = title;
-        this.content = content;
-        this.itemUrl = itemUrl;
+        this.title = title || '';
+        this.content = content || '';
+        this.itemUrl = itemUrl || '';
     }
 }
